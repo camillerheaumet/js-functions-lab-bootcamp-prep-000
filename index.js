44b5fdbea3331c53f1fc82dc14128a20c815b53b
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-07-22 16:17:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 function happyHolidays(string){
   return "Happy holidays!"
 }
+
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
